53e8d1b5a7e9c27de6b5ca409d89ee5423054f9e
Mattia46 <mattia.assogna@gmail.com>
2015-12-16 11:34:04 +0000

    added the post var and the show posts function

--- Twitter/MainViewController.swift
+++ Twitter/MainViewController.swift
@@ -12,6 +12,29 @@ class MainViewController: UITableViewController {
     
     var ref = Firebase(url: "https://twittermattia.firebaseio.com/")
     
+    
+    
+    
+    
+    var posts: [String] = ["ciao", "Julien"]
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return posts.count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell: UITableViewCell = self.tableView.dequeueReusableCellWithIdentifier("cell")!
+        cell.textLabel!.text = self.posts[indexPath.row]
+        return cell
+    }
+    
+    
+    
+    
+    
+    
+        
     override func viewDidLoad() {
         super.viewDidLoad()
     }
